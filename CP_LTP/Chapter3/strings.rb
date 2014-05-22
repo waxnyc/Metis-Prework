6503ef369a7c765c883c6f7e6568d2b04f4414b0
wax <jwaxnyc@gmail.com>
2014-05-22 14:48:12 -0400

    Chapter 3

---
+++ CP_LTP/Chapter3/strings.rb
@@ -0,0 +1,35 @@
+puts "Hello, world"
+puts ""
+puts "Good-bye."
+
+puts "I like" + "apple pie."
+
+puts "I like " + "apple pie."
+puts "I like" + " apple pie."
+
+puts 'blink ' * 4
+
+puts 12 + 12
+puts '12' + '12'
+puts "12" + "12"
+puts '12 + 12'
+puts "12 + 12"
+
+puts 2 * 5
+puts "2" * 5
+puts "2 * 5"
+
+puts "12" + 12
+puts "2" * "5"
+
+puts "Betty" + 12
+puts "Fred" * "John"
+
+puts "You're swell!"
+puts 'You're swell!'
+puts 'You\'re swell!'
+
+puts 'You\'re swell!
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
