d32c0f94c44aabb850c0318215d7e31092066089
wax <jwaxnyc@gmail.com>
2014-05-28 16:01:35 -0400

    Table of Contents exercise

---
+++ CP_LTP/Chapter6/ch6_table_of_contents.rb
@@ -0,0 +1,7 @@
+line_width = 36
+puts("Table of Contents".center(line_width))
+puts "" # use as a space
+puts "" # use as a space
+puts("Chapter 1: Getting Started".ljust(line_width) + "page 1".rjust(line_width/2))
+puts("Chapter 2: Numbers".ljust(line_width) + "page 9".rjust(line_width/2))
+puts("Chapter 3: Letters".ljust(line_width) + "page 13".rjust(line_width/2))
