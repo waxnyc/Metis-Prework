acc7df5cdd19a52350577c0ef058e7108c5af974
wax <jwaxnyc@gmail.com>
2014-05-28 15:38:17 -0400

    angryboss_exercise

---
+++ CP_LTP/Chapter6/ch6_angryboss.rb
@@ -0,0 +1,5 @@
+puts "Waxler! What the fuck do you want now? You sorry piece of donkey shit. GO AHEAD, TELL ME WHAT YOU WANT."
+answer = gets.chomp
+puts "WHAT THE...??? YOU DIMWITTED TWIT! YOU AND YOUR '#{answer}' CAN TAKE A LONG WALK OFF A SHORT PIER. YOU'RE FIRED!!!!"
+	
+
