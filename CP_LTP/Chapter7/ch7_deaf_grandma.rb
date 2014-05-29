7cf1a43e08d4bdd2d7db809e9d63b12410bb92e5
wax <jwaxnyc@gmail.com>
2014-05-29 13:36:25 -0400

    deaf_grandma fixed indentation

--- CP_LTP/Chapter7/ch7_deaf_grandma.rb
+++ CP_LTP/Chapter7/ch7_deaf_grandma.rb
@@ -1,17 +1,16 @@
-	puts "Atticus, please say something to your Grandma."
-	
-	while true
-		reply = gets.chomp
+puts "Atticus, please say something to your Grandma."
 
-		if reply == reply
-			puts "HUH?  SPEAK UP SONNY!"
-		else reply != reply.upcase
-			puts "NO, NOT SINCE 1938!"
+while true
+	reply = gets.chomp
+
+	if reply == reply
+		puts "HUH?  SPEAK UP SONNY!"
+	else reply != reply.upcase
+		puts "NO, NOT SINCE 1938!"
 	end
 
-		if reply == "BYE"
-			puts "Bye Grandma.  I'm going home now. I love you." 
-			break
-		end
+	if reply == "BYE"
+		puts "Bye Grandma.  I'm going home now. I love you." 
+		break
 	end
-	
+end
