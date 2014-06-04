d1ad80f66d727d4ccf7126ec1879d08f1f6484a6
wax <jwaxnyc@gmail.com>
2014-06-04 17:26:03 -0400

    TOC_array_take3 cut and print

--- CP_LTP/Chapter8/ch8_TOC_array.rb
+++ CP_LTP/Chapter8/ch8_TOC_array.rb
@@ -6,13 +6,16 @@ chapters = [["Getting Started", 1], ["Numbers", 9], ["Letters", 13]]
 puts title.center(line_width)
 puts "" #break
 
+chap_num = 1
+
 chapters.each do |chap, page|
   ch_name = chap
   page_num = page
 	
-  beginning = "#{ch_name}"
+  beginning = "Chapter #{chap_num}:  #{ch_name}"
   ending = "page #{page_num}"
 
   puts beginning.ljust(line_width) + ending.rjust(line_width/2)
+  chap_num = chap_num + 1
 end
 
