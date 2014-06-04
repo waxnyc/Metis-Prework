57ee2dccd405e2018ed690c1f8bedb0fda03db1d
wax <jwaxnyc@gmail.com>
2014-06-04 15:29:16 -0400

    Table of Contents as an array

---
+++ CP_LTP/Chapter8/ch8_TOC_array.rb
@@ -0,0 +1,20 @@
+line_width = 36 
+title = "Table of Contents"
+puts "" #break
+
+chapters = [["Getting Started", 1], ["Numbers", 9], ["Letters", 13]]
+puts "" #break
+
+puts title.center(line_width)
+puts "" #break
+
+chapters.each do |chap, page|
+	ch_name = chap
+	page_num = page
+	
+	beginning = "#{ch_name}"
+	ending = "page #{page_num}"
+
+	puts beginning.ljust(line_width) + ending.rjust(line_width/2)
+end
+
