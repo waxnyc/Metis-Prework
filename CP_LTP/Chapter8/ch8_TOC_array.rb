3d7df5ccf73d083f421c7fc8ca605db5297ba4a5
wax <jwaxnyc@gmail.com>
2014-06-04 16:37:25 -0400

    TOC as array_take3

--- CP_LTP/Chapter8/ch8_TOC_array.rb
+++ CP_LTP/Chapter8/ch8_TOC_array.rb
@@ -1,9 +1,7 @@
 line_width = 36 
 title = "Table of Contents"
-puts "" #break
 
 chapters = [["Getting Started", 1], ["Numbers", 9], ["Letters", 13]]
-puts "" #break
 
 puts title.center(line_width)
 puts "" #break
